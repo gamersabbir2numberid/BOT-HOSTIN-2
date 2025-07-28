30765c42905b5e72c5ae11a68d076a6f7a8d4f90
gamersabbir2numberid <insabbir661@gmail.com>
2025-07-28 19:30:14 +0600

    Update main.py

--- main.py
+++ main.py
@@ -188,8 +188,8 @@ async def like(interaction: discord.Interaction, uid: str, region: str):
 
     await interaction.response.defer()
 
-    url = f"https://like-api2-jult9y588-gamersabbirs-projects.vercel.app/{uid}&server_name={region}"
-    
+    url = f"https://like-api2-jult9y588-gamersabbirs-projects.vercel.app/like?uid={uid}&server_name={region}"
+
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url, timeout=10) as resp:
